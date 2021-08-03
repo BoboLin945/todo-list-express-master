da86382d8d96c9f26578cce68f5ba09c7f05e655
BoboLin <bobolincowo@gmail.com>
2021-08-03 15:41:15 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,47 @@
 const Todo = require('../todo')
+const User = require('../user')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
+  bcrypt.genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({
+          name: `name-${i}`, userId
+        })
+      ))
+      // Promise.all -> 非同步處理，保證 promise.all 中處理完再往下
+      // Promise.all([
+      //   Todo.create({ name: `name-0`, userId }),
+      //   Todo.create({ name: `name-1`, userId }),
+      //   .
+      //   .
+      //   .
+      // ])
 
-  console.log('done')
+    })
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
 })
